c00ddc6036f8307f120d7bb0a1fda5c57e5fd61d
Seonwoo Lee <dltjsdn1904@gmail.com>
2025-03-18 04:13:45 -0400

    added links

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Linking, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -11,6 +12,13 @@ export default function Index() {
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Text>hello</Text>
+
+      <Link href="/home">Home</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/sign-in">Sign In</Link>
+
+
     </View>
   );
 }
